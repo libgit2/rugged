0fde52d986e4ce2a1eec95d7c1ce43cdd468ba03
Scott Chacon <schacon@gmail.com>
2010-05-12 05:38:41 -0700

    benchmarks against pure-ruby Grit for object_exists checking

--- benchmarks.rb
+++ benchmarks.rb
@@ -1,15 +1,44 @@
 require 'benchmark'
+require 'rubygems'
+require 'grit'
+require 'pp'
 
 dir = File.dirname(File.expand_path(__FILE__))
 $LOAD_PATH.unshift dir + '/lib'
 require 'ribbit'
 
-n = 50000
+n = 5000
 
-sha = "ce08fe4884650f067bd5703b6a59a8b3b3c99a09"
+sha = "8496071c1b46c854b31185ea97743be6a8774479"
+psha = "41bc8c69075bbdb46c5c6f0566cc8cc5b46e8bd9"
 raw = Ribbit::Lib.hex_to_raw(sha)
 
+repo = File.dirname(File.expand_path(__FILE__)) + "/test/fixtures/testrepo.git"
+
 Benchmark.bm do |x|
+
+  @grit = Grit::Repo.new(repo).git
+  @ribbit = Ribbit::Odb.new(repo + '/objects')
+
+  x.report("Grit   object_exists? loose ") do
+    1.upto(n) { @grit.object_exists?(sha) }
+  end
+  x.report("Grit   object_exists? packed") do
+    1.upto(n) { @grit.object_exists?(psha) }
+  end
+  x.report("Ribbit object_exists? loose ") do
+    1.upto(n) { @ribbit.exists(sha) }
+  end
+  x.report("Ribbit object_exists? packed") do
+    1.upto(n) { @ribbit.exists(psha) }
+  end
+
+end
+
+n = 50000
+
+Benchmark.bm do |x|
+
   x.report("Ribbit Hex2Raw:") do
     1.upto(n) { Ribbit::Lib.hex_to_raw(sha) }
   end
