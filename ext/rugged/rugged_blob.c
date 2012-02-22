69ab181175692015dc212ddf21acb685d2d3354e
Brian Lopez <seniorlopez@gmail.com>
2012-02-21 18:16:10 -0800

    whitespace

--- ext/rugged/rugged_blob.c
+++ ext/rugged/rugged_blob.c
@@ -2,17 +2,17 @@
  * The MIT License
  *
  * Copyright (c) 2011 GitHub, Inc
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -111,7 +111,7 @@ static VALUE rb_git_blob_content_GET(int argc, VALUE *argv, VALUE self)
 
 	Data_Get_Struct(self, git_blob, blob);
 	rb_scan_args(argc, argv, "01", &rb_max_bytes);
-	
+
 	content = git_blob_rawcontent(blob);
 	size = git_blob_rawsize(blob);
 
