baf73f64d77454f55e3037a00e36f5d0b818fcc9
Zach Holman <zach@zachholman.com>
2012-04-23 16:25:19 +0100

    repo docs

--- lib/rugged/repository.rb
+++ lib/rugged/repository.rb
@@ -1,17 +1,30 @@
 module Rugged
+  # Repository is an interface into a Git repository on-disk. It's the primary
+  # interface between your app and the main Git objects Rugged makes available
+  # to you.
   class Repository
-
+    # Pretty formatting of a Repository.
+    #
+    # Returns a very pretty String.
     def inspect
       "#<Rugged::Repository:#{object_id} {path: #{path.inspect}}>"
     end
 
-    # Get the most recent commit from this repo
+    # Get the most recent commit from this repo.
     #
-    # Returns a Rugged::Commit object
+    # Returns a Rugged::Commit object.
     def last_commit
       self.lookup self.head.target
     end
 
+    # Walks over a set of commits using Rugged::Walker.
+    #
+    # from    - The String SHA1 to push onto Walker to begin our walk.
+    # sorting - The sorting order of the commits, as defined in the README.
+    # block   - A block that we pass into walker#each.
+    #
+    # Returns nothing if called with a block, otherwise returns an instance of
+    # Enumerable::Enumerator containing Rugged::Commit objects.
     def walk(from, sorting=Rugged::SORT_DATE, &block)
       walker = Rugged::Walker.new(self)
       walker.sorting(sorting)
@@ -19,20 +32,47 @@ module Rugged
       walker.each(&block)
     end
 
+    # Find the HEAD of this repository.
+    #
+    # Returns a Reference.
     def head
       ref = Reference.lookup(self, "HEAD")
       ref.resolve
     end
 
+    # Look up a SHA1.
+    #
+    # Returns one of the four classes that inherit from Rugged::Object.
     def lookup(oid)
       Rugged::Object.lookup(self, oid)
     end
 
-    # Look up a single reference by name
+    # Look up a single reference by name.
+    #
+    # Example:
+    #
+    #   repo.ref 'refs/heads/master'
+    #   # => #<Rugged::Reference:2199125780 {name: "refs/heads/master",
+    #          target: "25b5d3b40c4eadda8098172b26c68cf151109799"}>
+    #
+    # Returns a Rugged::Reference.
     def ref(ref_name)
       Rugged::Reference.lookup(self, ref_name)
     end
 
+    # Retuns all the References that match a pattern.
+    #
+    # refs - A Regexp (or String) to search for. Optional.
+    #
+    # Examples:
+    #
+    #   # All refs
+    #   repo.refs
+    #
+    #   # All remote refs
+    #   repo.refs 'refs/remotes'
+    #
+    # Returns an Array of References.
     def refs(pattern = nil)
       r = []
       ref_names.each do |ref_name|
@@ -46,18 +86,33 @@ module Rugged
       r
     end
 
+    # The names of all the refs.
+    #
+    # Returns an Enumerable::Enumerator containing all the String ref names.
     def ref_names
       Rugged::Reference.each(self)
     end
 
+    # All of the tags in the repository.
+    #
+    # Returns an Enumerable::Enumerator containing all the String tag names.
     def tags(pattern="")
       Rugged::Tag.each(self, pattern)
     end
 
+    # All of the remotes in the repository.
+    #
+    # Returns an Enumerable::Enumerator containing all the String remote names.
     def remotes
       Rugged::Remote.each(self)
     end
 
+    # Get the content of a file at a specific revision.
+    #
+    # revision - The String SHA1.
+    # path     - The String file path.
+    #
+    # Returns a String.
     def file_at(revision, path)
       tree = Rugged::Commit.lookup(self, revision).tree
       subtree = tree.get_subtree(path)
@@ -65,7 +120,5 @@ module Rugged
       blob = Rugged::Blob.lookup(self, blob_data[:oid])
       blob.content
     end
-
   end
-
 end
