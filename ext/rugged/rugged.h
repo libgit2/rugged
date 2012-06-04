a7863f3bebdfd68b9a57a8554246b4a035035b90
Brian Lopez <seniorlopez@gmail.com>
2012-06-04 10:53:51 -0700

    only deal with errors if there is one

--- ext/rugged/rugged.h
+++ ext/rugged/rugged.h
@@ -85,23 +85,25 @@ static inline VALUE rugged_owner(VALUE object)
 
 static inline void rugged_exception_check(int errorcode)
 {
-	switch(errorcode) {
-		case GIT_ENOTFOUND:
-			// return nil instaed of raising, should this be handled outside of this function?
-		case GIT_EEXISTS:
-			// file already exists
-		case GIT_EAMBIGUOUS:
-			// oid passed wasn't specific enough
-		case GIT_EBUFS:
-			// buffer wasn't big enough, shouldn't be raise do ruby land
-			// this should probably never happen given we use ruby strings
-		case GIT_ERROR:
-		default:
-			if (errorcode < 0)
-				rb_raise(rb_eRuntimeError, "%s\n(error code %d)", giterr_last()->message, errorcode);
-	}
+	if (errorcode < 0)
+		switch(errorcode) {
+			case GIT_ENOTFOUND:
+				// return nil instaed of raising, should this be handled outside of this function?
+			case GIT_EEXISTS:
+				// file already exists
+			case GIT_EAMBIGUOUS:
+				// oid passed wasn't specific enough
+			case GIT_EBUFS:
+				// buffer wasn't big enough, shouldn't be raise do ruby land
+				// this should probably never happen given we use ruby strings
+			case GIT_ERROR:
+			default:
+				if (errorcode < 0)
+					rb_raise(rb_eRuntimeError, "%s\n(error code %d)", giterr_last()->message, errorcode);
+		}
 
-	giterr_clear();
+		giterr_clear();
+	}
 }
 
 static inline int rugged_parse_bool(VALUE boolean)
