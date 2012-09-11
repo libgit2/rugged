ac964b889732fb2856f17cb2ab1a3a09e39aeb96
Brian Lopez <seniorlopez@gmail.com>
2012-09-11 09:59:17 -0700

    add Rugged::Diff::Delta#hunks enumerator

--- lib/rugged/diff/delta.rb
+++ lib/rugged/diff/delta.rb
@@ -12,6 +12,10 @@ module Rugged
       def inspect
         "#<#{self.class.name}:#{object_id} {old_file: #{old_file.inspect}, new_file: #{new_file.inspect}, similarity: #{similarity.inspect}, status: #{status.inspect}, binary: #{binary.inspect}>"
       end
+
+      def hunks
+        Enumerator.new(self, :each_hunk)
+      end
     end
   end
 end
