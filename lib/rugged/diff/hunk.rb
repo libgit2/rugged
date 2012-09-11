2c78ba241130dfc51c4fc5ca24f90a457f538236
Brian Lopez <seniorlopez@gmail.com>
2012-09-11 08:42:08 -0700

    add owner accessor and inspect to Rugged::Diff::Hunk

--- lib/rugged/diff/hunk.rb
+++ lib/rugged/diff/hunk.rb
@@ -2,7 +2,15 @@ module Rugged
   class Diff
     class Hunk
       attr_accessor :header
+
       attr_accessor :range
+
+      attr_accessor :owner
+      alias delta owner
+
+      def inspect
+        "#<#{self.class.name}:#{object_id} {header: #{header.inspect}, range: #{range.inspect}>"
+      end
     end
   end
 end
