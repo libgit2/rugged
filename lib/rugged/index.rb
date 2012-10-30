771b784f44ce20dbbed7eaca0e659743d3add253
Vicent Marti <tanoku@gmail.com>
2012-10-30 19:50:32 +0100

    Bring up to speed with the libgit2 index refactor branch

--- lib/rugged/index.rb
+++ lib/rugged/index.rb
@@ -1,5 +1,13 @@
 module Rugged
   class Index
     include Enumerable
+
+    def to_s
+      s = "#<Rugged::Index:0x1024bc2f8>\n"
+      self.each do |entry|
+        s << "  [#{entry[:stage]}] '#{entry[:path]}'\n"
+      end
+      s
+    end
   end
 end
