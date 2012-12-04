3a934cc2a3b8a3ef395b133588131486a50465cb
Brian Lopez <seniorlopez@gmail.com>
2012-12-04 11:55:56 -0800

    fix file_at test

--- test/repo_test.rb
+++ test/repo_test.rb
@@ -130,8 +130,8 @@ describe Rugged::Repository do
 
   it "can access a file" do
     sha = '36060c58702ed4c2a40832c51758d5344201d89a'
-    content = @repo.file_at(sha, 'new.txt')
-    assert_equal "new file\n", content
+    blob = @repo.blob_at(sha, 'new.txt')
+    assert_equal "new file\n", blob.content
   end
 
   it "can be garbage collected" do
