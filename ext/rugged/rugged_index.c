935d3e6b5eff97a47a943143925997b2e0c282bb
Ted Nyman <ted@ted.io>
2012-12-14 17:54:43 -0800

    Bit of whitespace cleanup

--- ext/rugged/rugged_index.c
+++ ext/rugged/rugged_index.c
@@ -2,17 +2,17 @@
  * The MIT License
  *
  * Copyright (c) 2011 GitHub, Inc
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -106,7 +106,6 @@ static VALUE rb_git_index_count(VALUE self)
 	Data_Get_Struct(self, git_index, index);
 	return INT2FIX(git_index_entrycount(index));
 }
-
 
 static VALUE rb_git_index_get(int argc, VALUE *argv, VALUE self)
 {
@@ -208,7 +207,7 @@ static VALUE rb_git_index_add(VALUE self, VALUE rb_entry)
 	}
 
 	else {
-		rb_raise(rb_eTypeError, 
+		rb_raise(rb_eTypeError,
 			"Expecting a hash defining an Index Entry or a path to a file in the repository");
 	}
 
