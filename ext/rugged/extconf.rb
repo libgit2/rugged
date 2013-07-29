a750ab8bfc8a388454e6eee5175d113fd75e4e1c
Arthur Schreiber <schreiber.arthur@googlemail.com>
2013-07-29 16:07:03 +0200

    Allow specifying the path to libgit2, but fallback to the included version.

--- ext/rugged/extconf.rb
+++ ext/rugged/extconf.rb
@@ -24,16 +24,19 @@ if MAKE_PROGRAM.nil?
   abort "ERROR: GNU make is required to build Rugged"
 end
 
-CWD = File.expand_path(File.dirname(__FILE__))
-LIBGIT2_DIR = File.join(CWD, '..', '..', 'vendor', 'libgit2')
-
-Dir.chdir(LIBGIT2_DIR) do
-  sys("#{MAKE_PROGRAM} -f Makefile.embed")
-end
-
-dir_config('git2', "#{LIBGIT2_DIR}/include", LIBGIT2_DIR)
+dir_config('git2')
 unless have_library 'git2' and have_header 'git2.h'
-  abort "ERROR: Failed to build libgit2"
+  CWD = File.expand_path(File.dirname(__FILE__))
+  LIBGIT2_DIR = File.join(CWD, '..', '..', 'vendor', 'libgit2')
+
+  Dir.chdir(LIBGIT2_DIR) do
+    sys("#{MAKE_PROGRAM} -f Makefile.embed")
+  end
+
+  dir_config('git2', "#{LIBGIT2_DIR}/include", LIBGIT2_DIR)
+  unless have_library 'git2' and have_header 'git2.h'
+    abort "ERROR: Failed to build libgit2"
+  end
 end
 
 create_makefile("rugged/rugged")
