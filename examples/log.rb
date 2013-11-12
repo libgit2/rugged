4ec36bea32c7ddc698e8d49d9302a600594aa7b3
Arthur Schreiber <schreiber.arthur@googlemail.com>
2013-11-12 21:56:01 +0100

    License examples under CC0.

--- examples/log.rb
+++ examples/log.rb
@@ -1,4 +1,16 @@
 #!/usr/bin/env ruby
+
+# Rugged "log" example - shows how to walk history and get commit info
+#
+# Written by the Rugged contributors, based on the libgit2 examples.
+#
+# To the extent possible under law, the author(s) have dedicated all copyright
+# and related and neighboring rights to this software to the public domain
+# worldwide. This software is distributed without any warranty.
+#
+# You should have received a copy of the CC0 Public Domain Dedication along
+# with this software. If not, see
+# <http://creativecommons.org/publicdomain/zero/1.0/>.
 $:.unshift File.expand_path("../../lib", __FILE__)
 
 require 'optparse'
