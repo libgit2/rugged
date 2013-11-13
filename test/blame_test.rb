7792e035d5859923b2455f0d39bf04142b8f41e1
Arthur Schreiber <schreiber.arthur@googlemail.com>
2013-11-13 19:31:11 +0100

    Add tests for blame options.

--- test/blame_test.rb
+++ test/blame_test.rb
@@ -54,6 +54,47 @@ class BlameTest < Rugged::SandboxedTestCase
     assert_nil @blame[-3]
   end
 
+  def test_blame_options
+    blame = Rugged::Blame.new(@repo, "branch_file.txt", max_line: 1)
+    assert_equal 1, blame.count
+
+    assert_equal({
+      :lines_in_hunk => 1,
+      :final_commit_id => "c47800c7266a2be04c571c04d5a6614691ea99bd",
+      :final_start_line_number => 1,
+      :final_signature => {
+        :name => "Scott Chacon",
+        :email => "schacon@gmail.com",
+        :time => Time.parse("2010-05-25 11:58:14 -0700")
+      },
+      :orig_commit_id => "0000000000000000000000000000000000000000",
+      :orig_path => "branch_file.txt",
+      :orig_start_line_number => 1,
+      :orig_signature => nil,
+      :boundary => false
+    }, blame[0])
+
+
+    blame = Rugged::Blame.new(@repo, "branch_file.txt", min_line: 2)
+    assert_equal 1, blame.count
+
+    assert_equal({
+      :lines_in_hunk => 1,
+      :final_commit_id => "a65fedf39aefe402d3bb6e24df4d4f5fe4547750",
+      :final_start_line_number => 2,
+      :final_signature => {
+        :name => "Scott Chacon",
+        :email => "schacon@gmail.com",
+        :time => Time.parse("2011-08-09 19:33:46 -0700")
+      },
+      :orig_commit_id => "0000000000000000000000000000000000000000",
+      :orig_path => "branch_file.txt",
+      :orig_start_line_number => 2,
+      :orig_signature => nil,
+      :boundary => false
+    }, blame[0])
+  end
+
   def test_blame_for_line
     assert_equal({
       :lines_in_hunk => 1,
