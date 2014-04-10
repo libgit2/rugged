65e396ed3549d468f1e657428aad65ff4ccd17cf
Hiroaki Nakamura <hnakamur@gmail.com>
2014-04-11 02:54:43 +0900

    Use assert ... instead of assert_equal true, ... and refute ... instead of assert_equal false, ...

--- test/repo_ignore_test.rb
+++ test/repo_ignore_test.rb
@@ -14,14 +14,14 @@ class RepositoryIgnoreTest < Rugged::SandboxedTestCase
   end
 
   def test_path_ignored
-    assert_equal true, @repo.path_ignored?("ign")
-    assert_equal false, @repo.path_ignored?("ignore_not")
-    assert_equal true, @repo.path_ignored?("dir")
-    assert_equal true, @repo.path_ignored?("dir/foo")
-    assert_equal true, @repo.path_ignored?("dir/foo/bar")
-    assert_equal false, @repo.path_ignored?("foo/dir")
-    assert_equal true, @repo.path_ignored?("foo/dir/bar")
-    assert_equal false, @repo.path_ignored?("direction")
+    assert @repo.path_ignored?("ign")
+    refute @repo.path_ignored?("ignore_not")
+    assert @repo.path_ignored?("dir")
+    assert @repo.path_ignored?("dir/foo")
+    assert @repo.path_ignored?("dir/foo/bar")
+    refute @repo.path_ignored?("foo/dir")
+    assert @repo.path_ignored?("foo/dir/bar")
+    refute @repo.path_ignored?("direction")
   end
 end
 
