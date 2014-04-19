395b04c7fedcec50942b095e9391746c58eb49e2
Arthur Schreiber <schreiber.arthur@gmail.com>
2014-04-19 21:47:25 +0200

    Refactor the online test cases to be a bit more DRY.

--- test/online/fetch_test.rb
+++ test/online/fetch_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class OnlineFetchTest < Rugged::SandboxedTestCase
+class OnlineFetchTest < Rugged::OnlineTestCase
   def setup
     super
     @repo = Rugged::Repository.init_at(File.join(@_sandbox_path, "repo"))
@@ -11,56 +11,33 @@ class OnlineFetchTest < Rugged::SandboxedTestCase
     super
   end
 
-  def ssh_creds?
-    %w{URL USER KEY PUBKEY PASSPHRASE}.all? { |key| ENV["GITTEST_REMOTE_SSH_#{key}"] }
+  if git_creds?
+    def test_fetch_over_git
+      Rugged::Remote.add(@repo, "origin", ENV['GITTEST_REMOTE_GIT_URL'])
+
+      @repo.fetch("origin")
+    end
   end
 
-  def git_creds?
-    ENV['GITTEST_REMOTE_GIT_URL']
+  if Rugged.features.include?(:https)
+    def test_fetch_over_https
+      Rugged::Remote.add(@repo, "origin", "https://github.com/libgit2/TestGitRepository.git")
+
+      @repo.fetch("origin")
+
+      assert_equal 5, @repo.refs.count
+      assert_equal [
+        "refs/remotes/origin/first-merge",
+        "refs/remotes/origin/master",
+        "refs/remotes/origin/no-parent",
+        "refs/tags/blob",
+        "refs/tags/commit_tree"
+      ], @repo.refs.map(&:name).sort
+    end
   end
 
-  def ssh_key_credential
-    Rugged::Credentials::SshKey.new({
-      username:   ENV["GITTEST_REMOTE_SSH_USER"],
-      publickey:  ENV["GITTEST_REMOTE_SSH_PUBKEY"],
-      privatekey: ENV["GITTEST_REMOTE_SSH_KEY"],
-      passphrase: ENV["GITTEST_REMOTE_SSH_PASSPHASE"],
-    })
-  end
-
-  def ssh_key_credential_from_agent
-    Rugged::Credentials::SshKeyFromAgent.new({
-      username: ENV["GITTEST_REMOTE_SSH_USER"]
-    })
-  end
-
-  def test_fetch_over_https
-    Rugged::Remote.add(@repo, "origin", "https://github.com/libgit2/TestGitRepository.git")
-
-    @repo.fetch("origin")
-
-    assert_equal 5, @repo.refs.count
-    assert_equal [
-      "refs/remotes/origin/first-merge",
-      "refs/remotes/origin/master",
-      "refs/remotes/origin/no-parent",
-      "refs/tags/blob",
-      "refs/tags/commit_tree"
-    ], @repo.refs.map(&:name).sort
-  end
-
-  def test_fetch_over_git
-    skip unless git_creds?
-
-    Rugged::Remote.add(@repo, "origin", ENV['GITTEST_REMOTE_GIT_URL'])
-
-    @repo.fetch("origin")
-  end
-
-  if Rugged.features.include? :ssh
+  if Rugged.features.include?(:ssh) && ssh_creds?
     def test_fetch_over_ssh_with_credentials
-      skip unless ssh_creds?
-
       Rugged::Remote.add(@repo, "origin", ENV['GITTEST_REMOTE_SSH_URL'])
 
       @repo.fetch("origin", {
@@ -69,8 +46,6 @@ class OnlineFetchTest < Rugged::SandboxedTestCase
     end
 
     def test_fetch_over_ssh_with_credentials_from_agent
-      skip unless ssh_creds?
-
       Rugged::Remote.add(@repo, "origin", ENV['GITTEST_REMOTE_SSH_URL'])
 
       @repo.fetch("origin", {
@@ -79,8 +54,6 @@ class OnlineFetchTest < Rugged::SandboxedTestCase
     end
 
     def test_fetch_over_ssh_with_credentials_callback
-      skip unless ssh_creds?
-
       Rugged::Remote.add(@repo, "origin", ENV['GITTEST_REMOTE_SSH_URL'])
 
       @repo.fetch("origin", {
