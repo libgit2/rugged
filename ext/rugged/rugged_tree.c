4379a54ed0bf959640e38f10a7a18ceea948c128
Brian Lopez <seniorlopez@gmail.com>
2015-11-30 16:46:02 -0800

    fixes for libgit2 api changes

--- ext/rugged/rugged_tree.c
+++ ext/rugged/rugged_tree.c
@@ -614,15 +614,15 @@ void rugged_parse_merge_options(git_merge_options *opts, VALUE rb_options)
 		}
 
 		if (RTEST(rb_hash_aref(rb_options, CSTR2SYM("renames")))) {
-			opts->tree_flags |= GIT_MERGE_TREE_FIND_RENAMES;
+			opts->flags |= GIT_MERGE_FIND_RENAMES;
 		}
 
 		if (RTEST(rb_hash_aref(rb_options, CSTR2SYM("fail_on_conflict")))) {
-			opts->tree_flags |= GIT_MERGE_TREE_FAIL_ON_CONFLICT;
+			opts->flags |= GIT_MERGE_FAIL_ON_CONFLICT;
 		}
 
 		if (RTEST(rb_hash_aref(rb_options, CSTR2SYM("skip_reuc")))) {
-			opts->tree_flags |= GIT_MERGE_TREE_SKIP_REUC;
+			opts->flags |= GIT_MERGE_SKIP_REUC;
 		}
 	}
 }
