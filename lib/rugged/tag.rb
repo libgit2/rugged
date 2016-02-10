15201cb0da4a8d494907c21dc3da783317b13549
Ben Toews <mastahyeti@users.noreply.github.com>
2016-02-10 09:35:57 -0700

    add Tag.extract_signature

--- lib/rugged/tag.rb
+++ lib/rugged/tag.rb
@@ -1,5 +1,24 @@
 module Rugged
   class Tag < Rugged::Reference
+    GPG_SIGNATURE_PREFIX = "-----BEGIN PGP SIGNATURE-----".freeze
+
+    def self.extract_signature(repo, oid, prefix=GPG_SIGNATURE_PREFIX)
+      object = repo.read(oid)
+
+      unless object.type == :tag
+        raise GitRPC::InvalidObject, "Invalid object type #{object.type}, expected tag"
+      end
+
+      unless index = object.data.index(prefix)
+        raise GitRPC::InvalidObject, "Tag does not contain signature"
+      end
+
+      [
+        object.data.byteslice(index..-1),
+        object.data.byteslice(0...index)
+      ]
+    end
+
     def name
       canonical_name.sub(%r{^refs/tags/}, "")
     end
