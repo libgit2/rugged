64a266dd141625fd6ae6dba0cec94e3312013f8b
Christian Schlack <christian@backhub.co>
2016-04-27 15:39:21 +0200

    Add counter parameter for all Rugged::Credentials classes

--- lib/rugged/credentials.rb
+++ lib/rugged/credentials.rb
@@ -20,7 +20,7 @@ module Rugged
         @username, @publickey, @privatekey, @passphrase = options[:username], options[:publickey], options[:privatekey], options[:passphrase]
       end
 
-      def call(url, username_from_url, allowed_types)
+      def call(url, username_from_url, allowed_types, counter)
         self
       end
     end
@@ -30,7 +30,7 @@ module Rugged
         @username = options[:username]
       end
 
-      def call(url, username_from_url, allowed_types)
+      def call(url, username_from_url, allowed_types, counter)
         self
       end
     end
@@ -38,7 +38,7 @@ module Rugged
     # A "default" credential usable for Negotiate mechanisms like NTLM or
     # Kerberos authentication
     class Default
-      def call(url, username_from_url, allowed_types)
+      def call(url, username_from_url, allowed_types, counter)
         self
       end
     end
