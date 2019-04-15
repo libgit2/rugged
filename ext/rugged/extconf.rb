libgit2/rugged

commit 790755c66a94f3b6088592367598706a97d9a530

Give CMake a timeout for when it hangs
In Debian Jessie, CMake will sometimes fail to correctly wait for the programs
it has spawned. While a proper fix would be in CMake itself, protect ourselves
from this by giving it a generous but limited time in which to run.

When it hangs, it will now instead be killed by the build process and we can try
again, instead of having to hunt down processes across machines.

ext/rugged/extconf.rb
@@ -4,6 +4,7 @@
 # For full terms see the included LICENSE file.
 
 require 'mkmf'
+require 'timeout'
 
 RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
 
@@ -21,6 +22,26 @@ def sys(cmd)
     raise "ERROR: '#{cmd}' failed"
   end
   ret
+end
+
+# Thrown when we detect CMake is taking too long and we killed it
+class CMakeTimeout < StandardError
+end
+
+def self.run_cmake(timeout, args)
+  # Set to process group so we can kill it and its children
+  pid = Process.spawn("cmake #{args}", pgroup: true)
+
+  Timeout.timeout(timeout) do
+    Process.waitpid(pid)
+  end
+
+rescue Timeout::Error
+  # Kill it, #detach is essentially a background wait, since we don't actually
+  # care about waiting for it now
+  Process.kill(-9, pid)
+  Process.detach(pid)
+  raise CMakeTimeout.new("cmake has exceeded its timeout of #{timeout}s")
 end
 
 MAKE = if Gem.win_platform?
@@ -80,7 +101,7 @@ else
     Dir.chdir("build") do
       # On Windows, Ruby-DevKit is MSYS-based, so ensure to use MSYS Makefiles.
       generator = "-G \"MSYS Makefiles\"" if Gem.win_platform?
-      sys("cmake .. -DBUILD_CLAR=OFF -DTHREADSAFE=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS=-fPIC -DCMAKE_BUILD_TYPE=RelWithDebInfo #{cmake_flags.join(' ')} #{generator}")
+      run_cmake(5 * 60, ".. -DBUILD_CLAR=OFF -DTHREADSAFE=ON -DBUILD_SHARED_LIBS=OFF -DCMAKE_C_FLAGS=-fPIC -DCMAKE_BUILD_TYPE=RelWithDebInfo #{cmake_flags.join(' ')} #{generator}")
       sys(MAKE)
 
       # "normal" libraries (and libgit2 builds) get all these when they build but we're doing it

commit 2f5a8f6c8f4ae9b94a2d1f6ffabc315f2592868d

fix broken Windows builds
First issue: pgroup is not available on Windows. new_pgroup should be
used instead. This broke in commit 790755c66a94f3b6088592367598706a97d9a530
(#763). Fixes #791.

Second issue: the mingw libssh2 package, which is required for building rugged,
is not installed out of the box on RubyInstaller with MSYS2.
This PR adds it to the dependency list so that installing the rugged gem
automatically pulls in the mingw libssh2 package as documented here:
https://github.com/oneclick/rubyinstaller2/wiki/For-gem-developers#-msys2-library-dependency
(related to PR #770).

ext/rugged/extconf.rb
@@ -30,7 +30,8 @@ end
 
 def self.run_cmake(timeout, args)
   # Set to process group so we can kill it and its children
-  pid = Process.spawn("cmake #{args}", pgroup: true)
+  pgroup = Gem.win_platform? ? :new_pgroup : :pgroup
+  pid = Process.spawn("cmake #{args}", pgroup => true)
 
   Timeout.timeout(timeout) do
     Process.waitpid(pid)

rugged.gemspec
@@ -31,4 +31,5 @@ desc
   s.add_development_dependency "rake-compiler", ">= 0.9.0"
   s.add_development_dependency "pry"
   s.add_development_dependency "minitest", "~> 5.0"
+  s.metadata["msys2_mingw_dependencies"] = "libssh2"
 end