c40f72928be9f8e7d3230853362785aa3a8aa426
Carlos Martín Nieto <cmn@dwim.me>
2021-09-01 09:09:20 +0200

    foo

--- lib/rugged/remote.rb
+++ lib/rugged/remote.rb
@@ -5,5 +5,39 @@
 
 module Rugged
   class Remote
+    # Try to connect to the +remote+. Useful to simulate
+    # <tt>git fetch --dry-run</tt> and <tt>git push --dry-run</tt>.
+    #
+    # Returns +true+ if connection is successful, +false+ otherwise.
+    #
+    # +direction+ must be either +:fetch+ or +:push+.
+    #
+    # The following options can be passed in as options:
+    #
+    # +credentials+ ::
+    #   The credentials to use for the connection. Can be either an instance of
+    #   one of the Rugged::Credentials types, or a proc returning one of the
+    #   former.
+    #   The proc will be called with the +url+, the +username+ from the url (if
+    #   applicable) and a list of applicable credential types.
+    #
+    # :headers ::
+    #   Extra HTTP headers to include with the request (only applies to http:// or https:// remotes)
+    #
+    # :proxy_url ::
+    #   The url of an http proxy to use to access the remote repository.
+    #
+    # Example:
+    #  remote = repo.remotes["origin"]
+    #  success = remote.check_connection(:fetch)
+    #  raise Error("Unable to pull without credentials") unless success
+    def check_connection(direction, **kwargs)
+      connect(direction, kwargs)
+      true
+    rescue
+      false
+    ensure
+      disconnect()
+    end
   end
 end
