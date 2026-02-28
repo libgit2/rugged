682f883cb3c164e117a6ff291bc7155bed03a88e
Daniel Berger <78529+djberg96@users.noreply.github.com>
2026-02-28 07:43:27 -0500

    Fix syntax errors.

--- ext/rugged/rugged_index.c
+++ ext/rugged/rugged_index.c
@@ -398,7 +398,7 @@ static VALUE rb_git_index_add_all(int argc, VALUE *argv, VALUE self)
 	error = git_index_add_all(index, &pathspecs, flags,
 		rb_block_given_p() ? rugged__index_matched_path_cb : NULL, &exception);
 
-	git_strarray_dispose(	xfree(pathspecs.strings);pathspecs);
+git_strarray_dispose(&pathspecs);
 
 	if (exception)
 		rb_jump_tag(exception);
@@ -448,7 +448,7 @@ static VALUE rb_git_index_update_all(int argc, VALUE *argv, VALUE self)
 	error = git_index_update_all(index, &pathspecs,
 		rb_block_given_p() ? rugged__index_matched_path_cb : NULL, &exception);
 
-	git_strarray_dispose(	xfree(pathspecs.strings);pathspecs);
+git_strarray_dispose(&pathspecs);
 
 	if (exception)
 		rb_jump_tag(exception);
@@ -494,7 +494,7 @@ static VALUE rb_git_index_remove_all(int argc, VALUE *argv, VALUE self)
 	error = git_index_remove_all(index, &pathspecs,
 		rb_block_given_p() ? rugged__index_matched_path_cb : NULL, &exception);
 
-	git_strarray_dispose(	xfree(pathspecs.strings);pathspecs);
+git_strarray_dispose(&pathspecs);
 
 	if (exception)
 		rb_jump_tag(exception);
@@ -695,7 +695,7 @@ static VALUE rb_git_diff_tree_to_index(VALUE self, VALUE rb_other, VALUE rb_opti
 	TypedData_Get_Struct(rb_other, git_tree, &rugged_object_type, other_tree);
 	error = git_diff_tree_to_index(&diff, repo, other_tree, index, &opts);
 
-	git_strarray_dispose(	xfree(opts.pathspec.strings);opts.pathspec);
+git_strarray_dispose(&opts.pathspec);
 	rugged_exception_check(error);
 
 	return rugged_diff_new(rb_cRuggedDiff, owner, diff);
@@ -718,7 +718,7 @@ static VALUE rb_git_diff_index_to_workdir(VALUE self, VALUE rb_options)
 
 	error = git_diff_index_to_workdir(&diff, repo, index, &opts);
 
-	git_strarray_dispose(	xfree(opts.pathspec.strings);opts.pathspec);
+git_strarray_dispose(&opts.pathspec);
 	rugged_exception_check(error);
 
 	return rugged_diff_new(rb_cRuggedDiff, owner, diff);
--- ext/rugged/rugged_repo.c
+++ ext/rugged/rugged_repo.c
@@ -2378,7 +2378,7 @@ static VALUE rb_git_checkout_tree(int argc, VALUE *argv, VALUE self)
 	rugged_parse_checkout_options(&opts, rb_options);
 
 	error = git_checkout_tree(repo, treeish, &opts);
-	git_strarray_dispose(	xfree(opts.paths.strings);opts.paths);
+git_strarray_dispose(&opts.paths);
 
 	if ((payload = opts.notify_payload) != NULL) {
 		exception = payload->exception;
@@ -2426,7 +2426,7 @@ static VALUE rb_git_checkout_index(int argc, VALUE *argv, VALUE self)
 	rugged_parse_checkout_options(&opts, rb_options);
 
 	error = git_checkout_index(repo, index, &opts);
-	git_strarray_dispose(	xfree(opts.paths.strings);opts.paths);
+git_strarray_dispose(&opts.paths);
 
 	if ((payload = opts.notify_payload) != NULL) {
 		exception = payload->exception;
@@ -2469,7 +2469,7 @@ static VALUE rb_git_checkout_head(int argc, VALUE *argv, VALUE self)
 	rugged_parse_checkout_options(&opts, rb_options);
 
 	error = git_checkout_head(repo, &opts);
-	git_strarray_dispose(	xfree(opts.paths.strings);opts.paths);
+git_strarray_dispose(&opts.paths);
 
 	if ((payload = opts.notify_payload) != NULL) {
 		exception = payload->exception;
